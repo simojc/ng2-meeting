85f58bf24f73f01a7d117309cee8d9ed318c0557
simocl2017 <simocl@yahoo.fr>
2019-01-13 14:02:51 -0500

    updates to deploy to heroku 3e fois apres erreur

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/ng2-meeting'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ng2-meeting/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+var port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Le serveur a démarré et écoute sur le port  ... " + port);
+})
